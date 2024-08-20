38caace02af2d811bca5ac4f06710fa49b29dcf4
Dev-Researcher <174758270+dev-researcher@users.noreply.github.com>
2024-08-20 14:02:34 -0600

    pyFiles

---
+++ scr/vertex_ai_client.py
@@ -0,0 +1,14 @@
+import requests 
+from config.settings import ENDPOINT, GOOGLE_CLOUD_API_KEY
+
+class VertexAIClient:
+    def __init__(self):
+        self.endpoint = ENDPOINT
+        self.headers = {
+            "Authorization" : f"Bearer {GOOGLE_CLOUD_API_KEY}",
+            "Content-Type" : "application/json",
+        }
+    def send_prompt(self, prompt) : data = {"instances" : [{"prompt": prompt}]}
+    response = requests.post(self.endpoint, headers=self.headers, json=data)
+
+return response.json()
