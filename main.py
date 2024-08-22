75091f12fbb533ad9fe461d0bdacd9e4c2558bbd
Dev-Researcher <174758270+dev-researcher@users.noreply.github.com>
2024-08-21 19:40:40 -0600

    final-change

--- main.py
+++ main.py
@@ -23,14 +23,13 @@ from vertexai.generative_models import GenerativeModel, ChatSession
 from config import PROJECT_ID, REGION, MODEL_NAME
 
 def initialize_model():
-    # Inicializa Vertex AI con el proyecto y la región correctos
     init(project=PROJECT_ID, location=REGION)
     model = GenerativeModel(model_name=MODEL_NAME)
     chat = model.start_chat()
     return chat
 
 def get_chat_response(chat: ChatSession, prompt: str) -> str:
-    # Envía un mensaje al modelo en modo streaming
+    # Envía like streaming
     text_response = []
     responses = chat.send_message(prompt, stream=True)
     for chunk in responses:
@@ -38,14 +37,14 @@ def get_chat_response(chat: ChatSession, prompt: str) -> str:
     return "".join(text_response)
 
 if __name__ == "__main__":
-    # Inicializa el modelo
+    # Inicia modelo
     chat = initialize_model()
 
-    # Envía algunos prompts de prueba
     prompts = [
-        "Hello.",
-        "What are all the colors in a rainbow?",
-        "Why does it appear when it rains?"
+        # "Hello.",
+        # "What are all the colors in a rainbow?",
+        # "Why does it appear when it rains?"
+        "componer una canción en español y usando las palabras como Tito que es un gatito, Darita mi sobrina"
     ]
     
     for prompt in prompts:
